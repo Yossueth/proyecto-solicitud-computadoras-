b567b27d47b442c86ff9be7dbc8c51aaa642262c
Yossueth <patricksarguedas2806@gmail.com>
2024-08-29 09:45:49 -0600

    camios

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,135 +1,96 @@
-
-
 import { getDatos, deleteDatos } from "../services/datos";
 
-
-
 function agregarDatosATabla(datos) {
+  const cuerpoTabla = document.getElementById("requestsBody");
+  cuerpoTabla.innerHTML = "";
 
-    
-    const cuerpoTabla = document.getElementById("requestsBody")
-    cuerpoTabla.innerHTML="";
-
-datos.forEach(item => {
-    
+  datos.forEach((item) => {
     const fila = document.createElement("tr");
-   
 
     const celdaFechaEntrada = document.createElement("td");
     celdaFechaEntrada.textContent = item.fechaEntrada;
     fila.appendChild(celdaFechaEntrada);
 
-
     const celdaFechaSalida = document.createElement("td");
-    celdaFechaSalida.textContent= item.fechaSalida;
+    celdaFechaSalida.textContent = item.fechaSalida;
     fila.appendChild(celdaFechaSalida);
 
-   
     const celdaCodeComputadora = document.createElement("td");
     celdaCodeComputadora.textContent = item.codePc;
     fila.appendChild(celdaCodeComputadora);
 
-    const celdaSede = document.createElement("td")
+    const celdaSede = document.createElement("td");
     celdaSede.textContent = item.sede;
     fila.appendChild(celdaSede);
-    
-    const celdaNombre = document.createElement("td")
+
+    const celdaNombre = document.createElement("td");
     celdaNombre.textContent = item.nombre;
     fila.appendChild(celdaNombre);
 
-    const celdaEstadoSolicitud = document.createElement("td")
+    const celdaEstadoSolicitud = document.createElement("td");
     celdaEstadoSolicitud.textContent = item.solicitud;
     fila.appendChild(celdaEstadoSolicitud);
 
-    const btnEliminar = document.createElement("button")
-    btnEliminar.innerHTML= "Eliminar"
+    const btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = "Eliminar";
     fila.appendChild(btnEliminar);
 
     cuerpoTabla.appendChild(fila);
 
-    btnEliminar.addEventListener("click",()=>{
-        deleteDatos(item.id)
-
-        .then(()=>{
-            fila.remove()
+    btnEliminar.addEventListener("click", () => {
+      deleteDatos(item.id)
+        .then(() => {
+          fila.remove();
         })
-        .catch((error)=> {
-            console.error("Error al eliminar", error)
-        })
-    })
-
-    
-
-
-});
-};
+        .catch((error) => {
+          console.error("Error al eliminar", error);
+        });
+    });
+  });
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const datos = await getDatos();
-        console.log(datos);
-        
-        if (datos) {
-            agregarDatosATabla(datos);
-        }
-    } catch (error) {
-        console.error('Error al obtener los datos:', error);
+  try {
+    const datos = await getDatos();
+    console.log(datos);
+
+    if (datos) {
+      agregarDatosATabla(datos);
     }
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+  }
 });
 
-
-
-
 const buscador = document.getElementById("buscador");
-const btnBuscar = document.getElementById("btnBuscar")
+const btnBuscar = document.getElementById("btnBuscar");
 
- 
 btnBuscar.addEventListener("click", filtrar);
 
 function filtrar() {
-   const valorBusqueda = buscador.value.toLowerCase();
-   
-   const filas = Array.from(document.querySelectorAll("#requestsBody tr"));
+  const valorBusqueda = buscador.value.toLowerCase();
 
-   console.log('Filas:', filas);
+  const filas = Array.from(document.querySelectorAll("#requestsBody tr"));
 
-   const filasFiltradas = filas.filter(fila => {
+  console.log("Filas:", filas);
+
+  const filasFiltradas = filas.filter((fila) => {
     const celdas = fila.querySelectorAll("td");
     let textoFila = "";
 
-    celdas.forEach(celda => {
-        textoFila += celda.textContent.toLocaleLowerCase() + " ";
-
-        
+    celdas.forEach((celda) => {
+      textoFila += celda.textContent.toLocaleLowerCase() + " ";
     });
-//Esto verifica que lo que esta en la fila sea verdadero o falso dependiendo la busqueda.
+    //Esto verifica que lo que esta en la fila sea verdadero o falso dependiendo la busqueda.
     return textoFila.includes(valorBusqueda);
-    
-   });
-   
-   filas.forEach(fila => {
-   fila.style.display = filasFiltradas.includes(fila) ? "" : "none";
+  });
 
-
-});
+  filas.forEach((fila) => {
+    fila.style.display = filasFiltradas.includes(fila) ? "" : "none";
+  });
 }
-
 
 const btnAtras = document.getElementById("btnAtras");
 btnAtras.addEventListener("click", function () {
-    window.location.href = ""
+  window.location.href = "";
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/js/login.js
+++ src/js/login.js
@@ -7,21 +7,31 @@ loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  let userList = await get() || [];
+  let userList = (await get()) || [];
 
-  let validUser = userList.find(
-    (user) => user.email === email && user.password === password
-  );
+  try {
+    if (!email.includes("@admin")) {
+      window.location.href = "http://localhost:1234/home.html";
+      return;
+    }
 
-  if (!validUser) {
-    texto.innerHTML = "Username and/or password are incorrect";
+    let validUser = userList.find(
+      (user) => user.email === email && user.password === password
+    );
 
-    setTimeout(() => {
-      texto.innerHTML = "";
-    }, 2000);
+    if (!validUser) {
+      texto.innerHTML = "Username and/or password are incorrect";
 
-    return;
+      setTimeout(() => {
+        texto.innerHTML = "";
+      }, 2000);
+
+      return;
+    }
+    
+    window.location.href = "http://localhost:1234/solicitudes.html";
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    texto.innerHTML = "An error occurred. Please try again later.";
   }
-
-  window.location.href = "http://localhost:1234/home.html";
 });
